57a8713f916887b74a7a5180f277f6c883262273
8osm <zareichuk.melnik@gmail.com>
2024-08-20 23:05:12 +0300

    test

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,14 +1,5 @@
 import {signIn} from "@/auth"
 
 export default function SignIn() {
-    return (
-        <form
-            action={async () => {
-                "use server"
-                await signIn("discord")
-            }}
-        >
-            <button type="submit">Signin with Discord</button>
-        </form>
-    )
+    return <button onClick={() => signIn()}>Sign in</button>
 }
